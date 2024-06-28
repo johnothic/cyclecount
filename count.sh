26bbbedab912facd5676ca3211114444b644eeb9
John Othic <165944869+johnothic@users.noreply.github.com>
2024-06-28 12:03:45 -0700

    Added V2 of shell file
    Added V2 of shell file that handles exception from Python that occurs when a users supplies more than 30 SKUs in their excel file

--- count.sh
+++ count.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+# create a copy of the cyclecount template and rename it for use with python
 cp countbckup.xlsx cyclecount.xlsx
-python3 cyclecount.py
-echo "spreadsheet updated"
+
+# Assign variable for error output when max number of SKUs is reached
+pyoutput=$(python3 cyclecount.py)
+
+# Print error if max number of SKU's is reached and script exits
+if [ "$pyoutput" == "max-sku-exceeded" ]
+then
+    echo "Maximum SKU count exceeded. Reduce to 30 or less."
+    echo "Script terminated."
+else
+    echo "Spreadsheet updated."
+fi
